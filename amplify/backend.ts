e58d7f096ae645bd151110d642b82fa3de8ba5f3
Timothy Lim <tlim0034@student.monash.edu>
2025-07-22 18:05:20 +1000

    yes

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,7 +3,7 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
 /**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and morey
  */
 defineBackend({
   auth,
